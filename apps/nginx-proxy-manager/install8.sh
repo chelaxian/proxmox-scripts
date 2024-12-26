e46fd2a03e08e9461b25ebc5224dc43d583aa309
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2024-12-26 19:21:31 +0300

    Update and rename install7.sh to install8.sh

--- apps/nginx-proxy-manager/install8.sh
+++ apps/nginx-proxy-manager/install8.sh
@@ -63,30 +63,18 @@ step_start "Rust" "Installing" "Installed"
   fi
 
   case "${EPS_OS_ARCH##*-}" in
-    amd64 | x86_64)
-      _rustArch="x86_64-unknown-linux-${_rustClibtype}"
-      ;;
-    arm64 | aarch64)
-      _rustArch="aarch64-unknown-linux-${_rustClibtype}"
-      ;;
-    armhf)
-      _rustArch="armv7-unknown-linux-gnueabihf"
-      ;;
-    i386 | x86)
-      _rustArch="i686-unknown-linux-${_rustClibtype}"
-      ;;
-    *)
-      echo "WARNING: Architecture not officially supported: ${EPS_OS_ARCH}. Using default architecture for Rust..."
-      _rustArch="x86_64-unknown-linux-${_rustClibtype}" # Значение по умолчанию для неподдерживаемых архитектур
-      ;;
+    amd64 | x86_64) _rustArch="x86_64-unknown-linux-${_rustClibtype}";;
+    arm64 | aarch64) _rustArch="aarch64-unknown-linux-${_rustClibtype}";;
+    armhf) _rustArch="armv7-unknown-linux-gnueabihf";;
+    i386 | x86) _rustArch="i686-unknown-linux-${_rustClibtype}";;
+    *) step_end "Architecture not supported: ${CLR_CYB}$EPS_OS_ARCH${CLR}" 1;;
   esac
 
-
-  #if [ "$EPS_OS_DISTRO" = "alpine" ]; then
-  #  if [ "$EPS_OS_ARCH" != "x86_64" -a "$EPS_OS_ARCH" != "aarch64" ]; then
-  #    step_end "Architecture not supported: ${CLR_CYB}$EPS_OS_ARCH${CLR}" 1
-  #  fi
-  #fi
+  if [ "$EPS_OS_DISTRO" = "alpine" ]; then
+    if [ "$EPS_OS_ARCH" != "x86_64" -a "$EPS_OS_ARCH" != "aarch64" ]; then
+      step_end "Architecture not supported: ${CLR_CYB}$EPS_OS_ARCH${CLR}" 1
+    fi
+  fi
   
   os_fetch -O ./rustup-init https://static.rust-lang.org/rustup/archive/1.26.0/$_rustArch/rustup-init
   chmod +x ./rustup-init
@@ -170,58 +158,40 @@ step_start "Openresty"
   step_end "Openresty ${CLR_CYB}v$OPENRESTY_VERSION${CLR} ${CLR_GN}Installed"
 
 step_start "Node.js"
-
-  # Определяем архитектуру
+  _nodePackage=""
   _nodeArch=""
+  _opensslArch="linux*"
+
   case "${EPS_OS_ARCH##*-}" in
-    amd64 | x86_64) _nodeArch="x64";;
-    arm64 | aarch64) _nodeArch="arm64";;
-    armhf) _nodeArch="armv7l";;
-    i386 | x86) _nodeArch="x86";;
-    *)
-      echo "WARNING: Unsupported architecture detected (${EPS_OS_ARCH}). Using default architecture (x64)..."
-      _nodeArch="x64"
-      ;;
+    amd64 | x86_64) _nodeArch="x64" _opensslArch="linux-x86_64";;
+    ppc64el) _nodeArch="ppc64le" _opensslArch="linux-ppc64le";;
+    s390x) _nodeArch="s390x" _opensslArch="linux-s390x";;
+    arm64 | aarch64) _nodeArch="arm64" _opensslArch="linux-aarch64";;
+    armhf) _nodeArch="armv7l" _opensslArch="linux-armv4";;
+    i386 | x86) _nodeArch="x86" _opensslArch="linux-elf";;
+    *) step_end "Architecture not supported: ${CLR_CYB}$EPS_OS_ARCH${CLR}" 1;;
   esac
 
-  # Проверяем, доступен ли Node.js в официальных репозиториях Alpine
-  echo "Checking for Node.js availability in Alpine repositories..."
-  if apk add nodejs npm; then
-    echo "Node.js successfully installed from Alpine repositories."
-  else
-    echo "Failed to install Node.js from Alpine repositories. Attempting manual installation..."
-
-    # Скачиваем Node.js из неофициальных сборок для Alpine (musl)
-    _nodePackage="node-$NODE_VERSION-linux-$_nodeArch-musl.tar.xz"
-    _nodeUrl="https://unofficial-builds.nodejs.org/download/release/$NODE_VERSION/$_nodePackage"
-
-    echo "Downloading Node.js from $_nodeUrl"
-    if ! wget -O $_nodePackage "$_nodeUrl"; then
-      echo "ERROR: Failed to download Node.js from $_nodeUrl."
-      step_end "Node.js installation failed" 1
+  if [ "$EPS_OS_DISTRO" = "alpine" ]; then
+    if [ "$_nodeArch" != "x64" ]; then
+      step_end "Architecture not supported: ${CLR_CYB}$EPS_OS_ARCH${CLR}" 1
     fi
 
-    # Распаковываем и устанавливаем Node.js
-    echo "Extracting Node.js..."
-    tar -xJf "$_nodePackage" -C /usr/local --strip-components=1 || {
-      echo "ERROR: Failed to extract Node.js package."
-      step_end "Node.js installation failed" 1
-    }
-
-    # Удаляем скачанный архив
-    rm "$_nodePackage"
-  fi
-
-  # Проверяем успешность установки
-  NODE_VERSION_INSTALLED=$(node -v 2>/dev/null || echo "Node.js not installed")
-  if [ "$NODE_VERSION_INSTALLED" != "Node.js not installed" ]; then
-    step_end "Node.js ${CLR_CYB}$NODE_VERSION_INSTALLED${CLR} ${CLR_GN}Installed"
+    _nodePackage="node-$NODE_VERSION-linux-$_nodeArch-musl.tar.xz"
+    os_fetch -O $_nodePackage https://unofficial-builds.nodejs.org/download/release/$NODE_VERSION/$_nodePackage
+    os_fetch -O SHASUMS256.txt https://unofficial-builds.nodejs.org/download/release/$NODE_VERSION/SHASUMS256.txt
   else
-    echo "ERROR: Node.js installation failed."
-    step_end "Node.js installation failed. Please check manually." 1
+    _nodePackage="node-$NODE_VERSION-linux-$_nodeArch.tar.xz"
+    os_fetch -O $_nodePackage https://nodejs.org/dist/$NODE_VERSION/$_nodePackage
+    os_fetch -O SHASUMS256.txt https://nodejs.org/dist/$NODE_VERSION/SHASUMS256.txt
   fi
 
-
+  grep " $_nodePackage\$" SHASUMS256.txt | sha256sum -c >$__OUTPUT
+  tar -xJf "$_nodePackage" -C /usr/local --strip-components=1 --no-same-owner >$__OUTPUT
+  ln -sf /usr/local/bin/node /usr/local/bin/nodejs
+  rm "$_nodePackage" SHASUMS256.txt
+  find /usr/local/include/node/openssl/archs -mindepth 1 -maxdepth 1 ! -name "$_opensslArch" -exec rm -rf {} \; >$__OUTPUT
+  step_end "Node.js ${CLR_CYB}$NODE_VERSION${CLR} ${CLR_GN}Installed"
 
 step_start "Yarn"
   export GNUPGHOME="$(mktemp -d)"
@@ -309,8 +279,7 @@ step_start "Frontend" "Building" "Built"
   export NODE_ENV=development
   yarn cache clean --silent --force >$__OUTPUT
   yarn install --silent --network-timeout=30000 >$__OUTPUT 
-  export NODE_OPTIONS=--openssl-legacy-provider
-  yarn build >$__OUTPUT
+  yarn build >$__OUTPUT 
   cp -r dist/* /app/frontend
   cp -r app-images/* /app/frontend/images
 
